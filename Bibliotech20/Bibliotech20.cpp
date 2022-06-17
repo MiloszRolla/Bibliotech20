ebd64c4fa62066dceef4f5bd51c1cb70274705c4
Maciej Henkel <maciek.henkel@gmail.com>
2022-06-17 21:36:25 +0200

    Fixed last commit, better now

--- Bibliotech20/Bibliotech20.cpp
+++ Bibliotech20/Bibliotech20.cpp
@@ -1,12 +1,29 @@
 #include "Book.h"
+#include <conio.h>
 using namespace std;
 void all_books();
 void available_books();
 void borrowed_books();
+
+
+#define ESCAPE 27
+#define ZERO 48
+#define ONE 49
+#define TWO 50
+#define THREE 51
+#define FOUR 52
+#define FIVE 53
+#define SIX 54
+#define SEVEN 55
+#define EIGHT 56
+#define NINE 57
+
+
 int main()
 {
+	setlocale(LC_CTYPE, "Polish");
 	Book b1, b2;
-	int selector;
+	int selector=0;
 	b1.title = "Przygody Toma Sawyera";
 	b1.author = "Pizdus Pan";
 	b1.is_borrowed = false;
@@ -18,37 +35,98 @@ int main()
 		<< "2. The list of available books\n"
 		<< "3. The list of borrowed books\n"
 		<< "To borrow a book please isit the list of available books and follow further instructions\n";
-	cin >> selector;
-	switch (selector)
-	{
-	case 1:
-	{
-		system("cls");
-		all_books();
-	}
-	case 2:
-	{
-		system("cls");
-		available_books();
-	}
-	case 3:
-		system("cls");
-		borrowed_books();
-	}
-
-
+		switch (selector = _getch())
+		{
+			case ONE:
+			{
+				system("cls");
+				all_books();
+				break;
+			}
+			case TWO:
+			{
+				system("cls");
+				available_books();
+				break;
+			}
+			case THREE:
+			{
+				system("cls");
+				borrowed_books();
+				break;
+			}
+			default:
+			{
+				system("cls");
+				cout << "You cannot do that\n";
+				main();
+			}
+		}
 	return 0;
-
 }
 void all_books()
 {
-	cout << "Z pliku sciaga liste ksiazek i wyswietla tytul, autora i stan, w kolorze zielonym lub czerwonym\n";
+	int a;
+	setlocale(LC_CTYPE, "Polish");
+	cout << "Z pliku sciaga liste ksiazek i wyswietla tytul, autora i stan, w kolorze zielonym lub czerwonym\n"
+		 <<"Aby wrocic wcisnij ESCAPE\n";
+	switch (a = _getch())
+	{
+		case ESCAPE:
+		{
+			system("cls");
+			main(); break;
+		}
+		default:
+		{
+			system("cls");
+			cout << "You cannot do that :c\n";
+			all_books();
+			break;
+		}
+	}
 }
 void available_books()
 {
-	cout << "Z pliku sciaga liste ksiazek o is_borrowed = 0, to je wyswietla\n";
+	int a;
+	setlocale(LC_CTYPE, "Polish");
+	cout << "Z pliku sciaga liste ksiazek o is_borrowed = 0, to je wyswietla\n"
+		 << "Aby wrocic wcisnij ESCAPE\n";
+	switch (a = _getch())
+	{
+		case ESCAPE:
+		{
+			system("cls");
+			main(); break;
+		}
+		default:
+		{
+			system("cls");
+			cout << "You cannot do that :c\n";
+			all_books();
+			break;
+		}
+	}
 }
 void borrowed_books()
 {
-	cout << "Z pliku sciaga liste ksiazek o is_borrowed = 1, to je wyswietla\n";
+	int a;
+	setlocale(LC_CTYPE, "Polish");
+	cout << "Z pliku œci¹ga liste ksiazek o is_borrowed = 1, to je wyswietla\n"
+	 	 << "Aby wrocic wcisnij ESCAPE\n";
+	switch (a = _getch())
+	{
+		case ESCAPE:
+		{
+			system("cls");
+			main(); break;
+		}
+		default:
+		{
+			system("cls");
+			cout << "You cannot do that :c\n";
+			all_books();
+			break;
+		}
+	}
 }
