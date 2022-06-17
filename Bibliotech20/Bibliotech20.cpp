0cba425ad7499f374b2a3dd4b716b6b041b0b1cf
Maciej Henkel <maciek.henkel@gmail.com>
2022-06-17 13:13:56 +0200

    Hardcode książek

--- Bibliotech20/Bibliotech20.cpp
+++ Bibliotech20/Bibliotech20.cpp
@@ -1,9 +1,14 @@
-#include <iostream>
-
+#include "Book.h"
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
+	Book b1, b2;
+	b1.title = "Przygody Toma Sawyera";
+	b1.author = "Pizdus Pan";
+	b1.is_borrowed=false;
+	b2.title = "Akademia Pana Kleksa";
+	b2.author = "Kapitan Ameryka";
+	b2.is_borrowed = true;
+	b1.show_book();
+	b2.show_book();
 }
